b075fc0cacf6612d42e25b4a5b187e5e80ba22d1
twinklelahariya1 <52948286+twinklelahariya1@users.noreply.github.com>
2019-08-11 16:17:41 +0530

    Add files via upload

---
+++ entities/Coins.java
@@ -0,0 +1,55 @@
+package entities;
+
+public class Coins {
+
+	private int black = 9;
+	private int red = 1;
+	private int striker = 1;
+
+	private int foulPoints = 0;
+
+	private static Coins object = null;
+
+	private Coins() {
+	}
+
+	public static Coins getInstance() {
+		if (object == null) {
+			object = new Coins();
+		}
+		return object;
+	}
+
+	public int getBlack() {
+		return black;
+	}
+
+	public void setBlack(int black) {
+		this.black = black;
+	}
+
+	public int getRed() {
+		return red;
+	}
+
+	public void setRed(int red) {
+		this.red = red;
+	}
+
+	public int getStriker() {
+		return striker;
+	}
+
+	public void setStriker(int striker) {
+		this.striker = striker;
+	}
+
+	public int getFoulPoints() {
+		return foulPoints;
+	}
+
+	public void setFoulPoints(int foulPoints) {
+		this.foulPoints = foulPoints;
+	}
+
+}
