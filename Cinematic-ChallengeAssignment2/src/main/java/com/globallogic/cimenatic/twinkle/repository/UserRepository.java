0581cc7a1cb2495522313a6bac91a2d0d5bc51bb
twinklelahariya1 <52948286+twinklelahariya1@users.noreply.github.com>
2019-08-18 11:55:25 +0530

    Delete UserRepository.java

--- Cinematic-ChallengeAssignment2/src/main/java/com/globallogic/cimenatic/twinkle/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.globallogic.cimenatic.twinkle.repository;
-
-import com.globallogic.cimenatic.twinkle.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
