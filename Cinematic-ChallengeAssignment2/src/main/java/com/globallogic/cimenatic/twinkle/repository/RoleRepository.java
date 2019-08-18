a03e4f60e494b452eb70216083c4756695a02242
twinklelahariya1 <52948286+twinklelahariya1@users.noreply.github.com>
2019-08-18 11:55:02 +0530

    Delete RoleRepository.java

--- Cinematic-ChallengeAssignment2/src/main/java/com/globallogic/cimenatic/twinkle/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.globallogic.cimenatic.twinkle.repository;
-
-import com.globallogic.cimenatic.twinkle.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
