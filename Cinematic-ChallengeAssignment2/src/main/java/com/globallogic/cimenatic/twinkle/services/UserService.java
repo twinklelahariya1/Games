e2d5e63b693db4abfae1b47350394906f21f61c1
twinklelahariya1 <52948286+twinklelahariya1@users.noreply.github.com>
2019-08-18 11:56:12 +0530

    Delete UserService.java

--- Cinematic-ChallengeAssignment2/src/main/java/com/globallogic/cimenatic/twinkle/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.globallogic.cimenatic.twinkle.services;
-
-import com.globallogic.cimenatic.twinkle.models.User;
-
-public interface UserService {
-    void save(User user);
-
-    User findByUsername(String username);
-}
